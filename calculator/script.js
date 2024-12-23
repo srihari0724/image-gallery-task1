af44a8a68124e807811a20e8ca496c2ae84f5f44
srihari0724 <sriharirenduchintala24@gmail.com>
2024-12-23 22:49:04 +0530

    `Removed entire calculator and portfolio projects, including HTML, CSS, and JavaScript files.`

---
+++ calculator/script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const lastChar = display.value.slice(-1);
+    if ('+-*/%'.includes(lastChar)) {
+        display.value = display.value.slice(0, -1) + operator;
+    } else {
+        display.value += operator;
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        alert('Invalid input!');
+    }
+}
